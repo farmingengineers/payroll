4f0597ed03f8888f14f922df03d2446bc0f886ee
Matt Burke <spraints@gmail.com>
2014-06-10 15:40:52 -0400

    Extract a method.

--- lib/timesheets.rb
+++ lib/timesheets.rb
@@ -7,38 +7,41 @@ module Timesheets
 
   class Parser
     def parse_raw(io, options = {})
-      log_io = options.fetch(:log) { NullIO.new }
-      entries = []
-      current_date = nil
-      current_name = nil
+      pc = Struct.
+        new(:log_io, :entries, :current_date, :current_name).
+        new(options.fetch(:log) { NullIO.new }, [], nil, nil)
       while !io.eof? && line = io.readline
-        log_io.puts "< #{line}"
-        line = line.strip
-        if line =~ /^[0-9\/-]+$/
-          current_date = Date.parse(line)
-        elsif line =~ /^([0-9\/-]+)\s+(\d{1,2}):(\d{1,2})[\s-]+(\d{1,2}):(\d{1,2})$/
-          entry = { :raw => line }
-          _, date, shr, smin, ehr, emin = $~.to_a
-          date = Date.parse(date)
-          entry[:name] = current_name
-          entry[:start], entry[:end] = mktimes(date, [shr, smin], [ehr, emin])
-          log_entry(log_io, entry)
-          entries << entry
-        elsif line =~ /^(.+)\s+(\d{1,2}):(\d{1,2})[\s-]+(\d{1,2}):(\d{1,2})$/
-          entry = { :raw => line }
-          _, name, shr, smin, ehr, emin = $~.to_a
-          entry[:name] = name
-          entry[:start], entry[:end] = mktimes(current_date, [shr, smin], [ehr, emin])
-          log_entry(log_io, entry)
-          entries << entry
-        else
-          current_name = line
-        end
+        parse_line(line, pc)
       end
-      entries
+      pc.entries
     end
 
     private
+
+    def parse_line(line, pc)
+      pc.log_io.puts "< #{line}"
+      line = line.strip
+      if line =~ /^[0-9\/-]+$/
+        pc.current_date = Date.parse(line)
+      elsif line =~ /^([0-9\/-]+)\s+(\d{1,2}):(\d{1,2})[\s-]+(\d{1,2}):(\d{1,2})$/
+        entry = { :raw => line }
+        _, date, shr, smin, ehr, emin = $~.to_a
+        date = Date.parse(date)
+        entry[:name] = pc.current_name
+        entry[:start], entry[:end] = mktimes(date, [shr, smin], [ehr, emin])
+        log_entry(pc.log_io, entry)
+        pc.entries << entry
+      elsif line =~ /^(.+)\s+(\d{1,2}):(\d{1,2})[\s-]+(\d{1,2}):(\d{1,2})$/
+        entry = { :raw => line }
+        _, name, shr, smin, ehr, emin = $~.to_a
+        entry[:name] = name
+        entry[:start], entry[:end] = mktimes(pc.current_date, [shr, smin], [ehr, emin])
+        log_entry(pc.log_io, entry)
+        pc.entries << entry
+      else
+        pc.current_name = line
+      end
+    end
 
     def mktimes(date, raw_start, raw_end)
       start_time = mktime(date, *raw_start)
