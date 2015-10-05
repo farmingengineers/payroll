2562a73cce9d800a0941d21a358d7b292e739c87
Matt Burke <spraints@gmail.com>
2015-10-04 20:37:28 -0400

    set up bower

---
+++ config/initializers/bower.rb
@@ -0,0 +1,3 @@
+# From http://dotwell.io/taking-advantage-of-bower-in-your-rails-4-app/
+# This might need to go in config/application.rb instead
+config.assets.paths << Rails.root.join("vendor/assets/components")
