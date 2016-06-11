0dcca349b3595358180700eeeb409a8e9aee07f8
Matt Burke <spraints@gmail.com>
2016-06-11 19:45:43 -0400

    Require exactly one match in NameCompleter

--- lib/name_completer.rb
+++ lib/name_completer.rb
@@ -1,9 +1,18 @@
 class NameCompleter
+  class NotFound < StandardError; end
+  class Ambiguous < StandardError; end
+
   def initialize(options)
     @full_names = options.fetch(:full_names)
   end
 
   def lookup(name)
-    @full_names.find { |n| n.start_with?(name) }
+    dname = name.downcase
+    matches = @full_names.select { |n| n.downcase.start_with?(dname) }
+    raise NotFound, "#{name.inspect} is not in the list!" \
+      if matches.empty?
+    raise Ambiguous, "#{name.inspect} matched more than one (#{matches.inspect})" \
+      if matches.size > 1
+    matches.first
   end
 end
--- test/name_completer_test.rb
+++ test/name_completer_test.rb
@@ -5,16 +5,32 @@ require_relative '../lib/name_completer'
 class NameCompleterTest < MiniTest::Unit::TestCase
   def test_empty_name_list
     nc = NameCompleter.new :full_names => []
-    assert_equal "Example Name", nc.lookup("Example Name")
+    assert_raises NameCompleter::NotFound do
+      nc.lookup("Example Name")
+    end
   end
 
   def test_name_not_in_list
     nc = NameCompleter.new :full_names => ["In List"]
-    assert_equal "Not", nc.lookup("Not")
+    assert_raises NameCompleter::NotFound do
+      nc.lookup("Not")
+    end
   end
 
   def test_first_name_in_list
     nc = NameCompleter.new :full_names => ["Mel Adams", "Joe Bob"]
     assert_equal "Joe Bob", nc.lookup("Joe")
+  end
+
+  def test_first_name_in_list_with_different_case
+    nc = NameCompleter.new :full_names => ["Mel Adams", "Joe Bob"]
+    assert_equal "Joe Bob", nc.lookup("joe")
+  end
+
+  def test_more_than_one_match
+    nc = NameCompleter.new :full_names => ["Mel Adams", "Mel Bob"]
+    assert_raises NameCompleter::Ambiguous do
+      nc.lookup("Mel")
+    end
   end
 end
